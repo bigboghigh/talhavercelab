cffc44fe09fefc853551556114986092d1796f63
Muhammad Talha <talhariaz5425869@gmail.com>
2023-07-27 13:38:48 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,49 @@
-const express = require('express')
-const app = express()   
-const  {getMb,checkData,receiveMb} = require('./jazz.js')
-require('dotenv').config()
-const port = process.env.PORT|| 5001
-app.use(express.json())
-app.use(express.static('./public'))
-app.get('/getmb/:msisdn',async(req,res)=>{
-    const {msisdn} = req.params
-    const response = await getMb(msisdn)
-    res.json({res:response}).status(200)
-})
-app.get('/receivemb/:msisdn',async(req,res)=>{
-    const {msisdn} = req.params
-    const response = await receiveMb(msisdn)
-    res.json({res:response}).status(200)
-})
-app.get('/checkmb/:msisdn',async(req,res)=>{
-    const {msisdn} = req.params
-    const response = await checkData(msisdn)
-    res.json({res:response}).status(200)
-})
-app.listen(port,()=>{
-    console.log(`running at port:${port}`)
-})
+const express = require('express');
+const app = express();
+const { getMb, checkData, receiveMb } = require('./jazz.js');
+require('dotenv').config();
+const port = process.env.PORT || 5001;
+
+app.use(express.json());
+app.use(express.static('./public'));
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Something went wrong!' });
+});
+
+app.get('/getmb/:msisdn', async (req, res, next) => {
+  try {
+    const { msisdn } = req.params;
+    const response = await getMb(msisdn);
+    res.json({ res: response }).status(200);
+  } catch (err) {
+    next(err); // Pass the error to the error handling middleware
+  }
+});
+
+app.get('/receivemb/:msisdn', async (req, res, next) => {
+  try {
+    const { msisdn } = req.params;
+    const response = await receiveMb(msisdn);
+    res.json({ res: response }).status(200);
+  } catch (err) {
+    next(err); // Pass the error to the error handling middleware
+  }
+});
+
+app.get('/checkmb/:msisdn', async (req, res, next) => {
+  try {
+    const { msisdn } = req.params;
+    const response = await checkData(msisdn);
+    res.json({ res: response }).status(200);
+  } catch (err) {
+    next(err); // Pass the error to the error handling middleware
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Running at port: ${port}`);
+});
+        
