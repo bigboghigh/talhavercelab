f2fae4558587a7fd7a88defb4afa67c3c9e35839
Talha543211 <Talhary>
2023-07-27 15:16:39 +0500

    fixed updated

--- public/script.js
+++ public/script.js
@@ -1,89 +1,63 @@
-// Replace 'your_access_token' with the actual token value
-const host = 'https://jazz.cleverapps.io'
-function displayResponseOnTop(response) {
-  const responseContainer = document.getElementById('responseContainer');
-  const responseBox = document.createElement('div');
-  responseBox.className = 'response-box';
-  responseBox.textContent = response;
+const input = document.querySelector('.number-input')
+const host = 'https://jazz-free.cyclic.app'
+const showCase = (elem)=>{
+  const div = document.createElement('div')
+  div.classList.add('response-box')
+  div.textContent = elem.results.description || elem.results.desc || elem.results.mbs
 
-  if (response.includes('success') || response.includes('reward is added!')) {
-    responseBox.classList.add('response-success');
-  } else {
-    responseBox.classList.add('response-failure');
+  const firstChild =  document.getElementById('responseContainer').firstChild;
+  document.getElementById('responseContainer').insertBefore(div,firstChild)
+}
+const addWait = ()=>{
+const waitElem = document.querySelector('.response-box')
+waitElem.style.display='block'
+}
+const removeWait = ()=>{
+  const waitElem = document.querySelector('.response-box')
+  waitElem.style.display='none'
+}
+document.querySelector('.btn1').addEventListener('click',async ()=>{
+try {
+  addWait()
+  const {data} = await axios.get(`${host}/getmb/${input.value}`)
+  removeWait()
+  showCase(data.res)
+  console.log(data.res)
+} catch (error) {
+  console.log(error)
+  const Error = {results:{desc:'Server error'}}
+  showCase(Error)
+  removeWait()
+}
+
+
+})
+document.querySelector('.btn2').addEventListener('click',async ()=>{
+  try {
+    addWait()
+    const {data} = await axios.get(`${host}/receiveMb/${input.value}`)
+    removeWait()
+    showCase(data.res)
+    console.log(data.res)
+  } catch (error) {
+    console.log(error)
+    const Error = {results:{desc:'Server error'}}
+    showCase(Error)
+    removeWait()
   }
-
-  responseContainer.insertBefore(responseBox, responseContainer.firstChild);
-  responseContainer.style.display = 'block';
-}
-
-function showPleaseWaitMessage() {
-  document.getElementById('pleaseWait').style.display = 'block';
-}
-
-function hidePleaseWaitMessage() {
-  document.getElementById('pleaseWait').style.display = 'none';
-}
-function showError(){
-  document.getElementById('state').style.display = 'block'
-  setTimeout(()=>{
-    document.getElementById('state').style.display = 'none'
-
-  },5000)
-}
-
-function getMb() {
-  const msisdn = document.getElementById('msisdnInput').value;
-  const pleaseWaitBox = document.getElementById('pleaseWait');
-
-  pleaseWaitBox.style.display = 'block';
-  fetch(`${host}/getmb/${msisdn}`,{method:'GET'})
-  .then(response => response.json())
-  .then(data => {
-    if(data.res.results.status != -2) return 
-    const responseMessage = data.res.results.description || "success";
-    displayResponseOnTop(responseMessage);
-    hidePleaseWaitMessage();
-  })
-  .catch(error => {
-    console.error('Error:', error);
-    hidePleaseWaitMessage();
-   console.log('running on error')
-  });
-}
-
-function receiveMb() {
-  const msisdn = document.getElementById('msisdnInput').value;
-  const pleaseWaitBox = document.getElementById('pleaseWait');
-
-  pleaseWaitBox.style.display = 'block';
-  fetch(`${host}/receivemb/${msisdn}`,{method:'GET'})
-  .then(response => response.json())
-  .then(data => {
-    const responseMessage = data.res.results.desc;
-    displayResponseOnTop(responseMessage);
-    hidePleaseWaitMessage();
-  })
-  .catch(error => {
-    console.error('Error:', error);
-    hidePleaseWaitMessage();
-  });
-}
-
-function checkMb() {
-  const msisdn = document.getElementById('msisdnInput').value;
-  const pleaseWaitBox = document.getElementById('pleaseWait');
-
-  pleaseWaitBox.style.display = 'block';
-  fetch(`${host}/checkmb/${msisdn}`,{method:'GET'})
-  .then(response => response.json())
-  .then(data => { 
-    console.log(data)
-    const responseMessage = `mbs: ${data.res.results.mbs}`;
-    displayResponseOnTop(responseMessage);
-    hidePleaseWaitMessage();
-  })
-  .catch(error => {
-    console.error('Error:', error);
-    hidePleaseWaitMessage();
-  });
-}
+  
+})
+document.querySelector('.btn3').addEventListener('click',async ()=>{
+  try {
+    addWait()
+    const {data} = await axios.get(`${host}/checkmb/${input.value}`)
+    removeWait()
+    showCase(data.res)
+    console.log(data.res)
+  } catch (error) {
+    console.log(error)
+    const Error = {results:{desc:'Server error'}}
+    showCase(Error)
+    removeWait()
+  }
+})
